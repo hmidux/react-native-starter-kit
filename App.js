6fb1c9b68e95adf742b4dec89d7a9ac53b0ae8e9
Hameir H'mida <hameir.hmida@gmail.com>
2022-11-23 02:24:26 +0100

    react-native + Tab navigation et Stack navigation

---
+++ App.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Bienvenue sur Twitter (by Hmida & Jocelyn)</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
